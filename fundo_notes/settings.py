25aacfd297b608c6aaffe41879b3bac5c960e76a
shreegowri <gaganandh@gmail.com>
2022-10-02 07:42:09 +0530

    [shreegowri]implementing the rabbitMQ server to send the mail

--- fundo_notes/settings.py
+++ fundo_notes/settings.py
@@ -138,8 +138,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'maheshkodergowri@gmail.com'
-EMAIL_HOST_PASSWORD = 'gjybeclxdvyhydzl'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 BASE_URL='http://127.0.0.1:8000'
 
